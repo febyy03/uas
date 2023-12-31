42d8dcc0c9b6f96e1e402f0944831568523721b2
unknown <ekafebriatiningsih003@gmail.com>
2023-12-31 21:13:01 +0700

    push yang ketiga dan finish

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,11 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.first, name='first'),
-    path('website/', views.website, name ='website'),
+    path('amed/', views.amed, name='amed'),
+    path('pandawa/', views.pandawa, name='pandawa'),
+    path('melasti/', views.melasti, name='melasti'),
+    path('kuta/', views.kuta, name='kuta'),
+    path('jimbaran/', views.jimbaran, name='jimbaran'),
+    #path('', views.first, name='first'),
+    #path('website/', views.website, name ='website'),
 ]
--- website/views.py
+++ website/views.py
@@ -8,5 +8,17 @@ def website(request):
     return HttpResponse(template.render())
 def first(request):
     return render(request, 'first.html')
+
+# class pantai====================================================
 def home(request):
     return render(request, 'home.html')
+def amed(request):
+    return render(request, 'amed.html')
+def pandawa(request):
+    return render(request, 'pandawa.html')
+def melasti(request):
+    return render(request, 'melasti.html')
+def kuta(request):
+    return render(request, 'kuta.html')
+def jimbaran(request):
+    return render(request, 'jimbaran.html')
